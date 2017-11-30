675855899cf6f038da718a395b2f189e31ae713c
Jenny Johnson <jennyejohnson94@gmail.com>
2017-11-30 09:46:58 +0000

    left and right working

--- main.js
+++ main.js
@@ -16,9 +16,42 @@ $(function () {
       updownmid(1, 0);
       updown(2, 0);
     }
+
+    if (event.keyCode == 39) {
+      console.log("right");
+      leftrightmid(1, 2);
+      leftright(0, 2);
+    }
+
+    if (event.keyCode == 37) {
+      console.log("left");
+      leftrightmid(1, 0);
+      leftright(2, 0);
+    }
   });
 
 });
+
+function leftright (start, end) {
+  grid[start].forEach(function (x, i) {
+    if ((grid[i][1].innerHTML == 0) && (grid[i][end].innerHTML == 0)) {
+      grid[i][end].innerHTML = grid[i][start].innerHTML;
+      grid[i][start].innerHTML = 0;
+    } else if (grid[i][1].innerHTML == 0) {
+      grid[i][1].innerHTML = grid[i][start].innerHTML;
+      grid[i][start].innerHTML = 0;
+    }
+  });
+}
+
+function leftrightmid(start, end) {
+  grid[start].forEach(function (x, i) {
+    if (grid[i][end].innerHTML == 0) {
+      grid[i][end].innerHTML = grid[i][start].innerHTML;
+      grid[i][start].innerHTML = 0;
+    }
+  });
+}
 
 function updown (start, end) {
   grid[start].forEach(function (x, i) {
@@ -32,7 +65,7 @@ function updown (start, end) {
   });
 }
 
-function updownmid(start, end, i) {
+function updownmid(start, end) {
   grid[start].forEach(function (x, i) {
     if (grid[end][i].innerHTML == 0) {
       grid[end][i].innerHTML = grid[start][i].innerHTML;
